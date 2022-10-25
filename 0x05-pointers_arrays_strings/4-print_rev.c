b500973fd91e3b29ae8b3ba53c7f6208914d1523
Fiimai <cris.sarbah@gmail.com>
2022-10-25 01:26:26 +0000

    Almost reversed for life

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - a funtion that prints a string, and in reverse,
+* also followed by a new line
+* @str: an input string
+* Return: Nothing
+*/
+void print_rev(char *g)
+{
+	int len = 0;
+
+	while (g[len] != '\0')
+		len++;
+
+	while (len)
+		_putchar(g[--len]);
+
+	_putchar('\n');
+}
