92ab02c729a55202d5cca44e588beaa09e01494e
Fiimai <cris.sarbah@gmail.com>
2022-11-07 23:49:27 +0000

    Print the number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
-#include  <stdio.h>
-/**
-* main: - This prints the number of arguments passed to it.
-* @argc: This refers to the number of command line arguments.
-* @argv: This refers to the command line arguments.
-* Return: 0 - means success.
-**/
+#include "main.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[]__attribute__((unused)))
+/**
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	if (argv[0])
+		printf("%d\n", argc - 1);
 	return (0);
 }
